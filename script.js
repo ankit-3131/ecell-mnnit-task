5a195bd871e66ef29549df7ba12fbb336c7651bf
ankit-3131 <heyyynkit@gmail.com>
2025-01-22 22:09:28 +0530

    working hamburger for smartphones

--- script.js
+++ script.js
@@ -23,3 +23,48 @@ function reset() {
     });
     seq = [];
 }
+
+function openHamburger(){
+    let existingStyle = document.getElementById("hamburger-style");
+    const parent = document.head;
+    if (existingStyle) {
+        parent.removeChild(existingStyle);
+    } else {
+    
+        const style = document.createElement("style");
+        style.id = "hamburger-style";
+
+        style.innerHTML = `@media (max-width:430px) {
+        .nav-bar a{
+            display: block;
+        }
+            .nav-bar-right{
+        height: 15vh;
+        margin: 10px;
+        justify-content: space-between;
+        align-items: center;
+        flex-direction: column;
+
+    }
+    .nav-bar{
+        height: 18vh;
+        position: absolute;
+    }
+    .nav-bar a{
+        position: relative;
+        right: 30%;
+    }
+    .line-container{
+        position: relative;
+        left: 35%;
+    }
+    
+        }`;
+
+        parent.appendChild(style);    
+        console.log('cliccked')
+    }   
+
+}
+const hamburger = document.querySelector(".line-container");
+hamburger.addEventListener('click', openHamburger);
