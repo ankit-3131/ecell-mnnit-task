57b8949d43ad9df28d7a4e18ec1223b62f03ffaa
ankit-3131 <heyyynkit@gmail.com>
2025-01-22 20:05:15 +0530

    colour grid working

---
+++ script.js
@@ -0,0 +1,25 @@
+let buttons = document.querySelectorAll(".options");
+let seq = [];
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        bgred(btn);
+        if (btn.id ==="option9") {
+            reset();
+        }
+    });
+});
+
+function bgred(btn) {
+    btn.classList.add("bg-red");
+    seq.push(btn);
+}
+
+function reset() {
+    seq.forEach((btn, index) => {
+        setTimeout(() => {
+            btn.classList.remove("bg-red");
+        }, 500 * (index + 1));
+    });
+    seq = [];
+}
